6eb0e791545113f44f9d47ae950170eafde46cb6
Dimas Lesmana <dimas.lesmana@student.umn.ac.id>
2020-12-06 16:17:51 +0700

    update App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React from 'react';
 import './App.css';
+import { Container, Typography, makeStyles } from '@material-ui/core';
+import Countdown from './components/Countdown';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    marginTop: '50px',
+    textAlign: 'center',
+  },
+  header: {
+    backgroundColor: '#f57c00',
+    borderRadius: '15px'
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <h1>App</h1>
+    <Container maxWidth="sm" className={classes.root}>
+      <div className={classes.header}>
+        <Typography variant="h3" align="center" gutterBottom>
+          <strong>SIMPLE COUNTDOWN</strong>
+        </Typography>
+      </div>
+      <Countdown />
+    </Container>
   );
 }
 
